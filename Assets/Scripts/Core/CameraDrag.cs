a19d3f9a030c22208ac39ddc9afe6e2a24f2f460
JlordS32 <jlordsty128@gmail.com>
2025-06-28 10:49:34 +1000

    organising & added camera move on mouse drag

---
+++ Assets/Scripts/Core/CameraDrag.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    // PROPERTIES
+    [SerializeField] float _dragSpeed = 2f;
+    [SerializeField] float _inertiaDuration = 0.5f;
+    [SerializeField] float _inertiaDamping = 5f;
+
+    // VARIABLES
+    Vector3 _dragOrigin;
+    Vector3 _velocity;
+    bool _isDragging = false;
+    float _inertiaTime = 0;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _dragOrigin = Input.mousePosition;
+            _isDragging = true;
+            _velocity = Vector3.zero;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = Camera.main.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
+            Vector3 move = new(-difference.x * _dragSpeed, -difference.y * _dragSpeed, 0);
+            transform.Translate(move, Space.World);
+            _velocity = move / Time.deltaTime;
+            _dragOrigin = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isDragging = false;
+            _inertiaTime = 0;
+        }
+
+        if (!_isDragging && _velocity.magnitude > 0.01f)
+        {
+            _inertiaTime += Time.deltaTime;
+            float t = 1 - Mathf.Clamp01(_inertiaTime / _inertiaDuration);
+            Vector3 move = _velocity * t * Time.deltaTime;
+            transform.Translate(move, Space.World);
+            _velocity = Vector3.Lerp(_velocity, Vector3.zero, Time.deltaTime * _inertiaDamping);
+        }
+
+        // Freeze z
+        Vector3 pos = transform.position;
+        pos.z = 0;
+        transform.position = pos;
+    }
+}
