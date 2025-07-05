60ec636f7d7654f256f242aa398090ca0f3c2e67
JlordS32 <jlordsty128@gmail.com>
2025-07-05 18:43:42 +1000

    working on enemy attack logic

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,16 +1,55 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+// TODO: Enemy Attacks Player
+// TODO: Enemy Takes Damage
+public class Enemy : MonoBehaviour, IAttack
 {
+    // PROPERTIES
+    [SerializeField] float _coolDown;
+    [SerializeField] float _damage = 5f;
+
+    // REFERENCES
+    Animator _anim;
+
+    // VARIABLES
+    float _timer;
+
+    void Awake()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
     void OnEnable() => EnemyManager.Register(this);
     void OnDisable() => EnemyManager.Unregister(this);
 
-    void OnTriggerEnter2D(Collider2D collision)
+    IDamageable _targetInRange;
+
+    void Update()
     {
-        if (collision.CompareTag("Player") && collision.TryGetComponent<Player>(out var player))
+        _timer += Time.deltaTime;
+
+        if (_targetInRange != null && _timer >= _coolDown)
         {
-            player.TakeDamage(10f);
+            Attack(_targetInRange);
+            _timer = 0f;
         }
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.TryGetComponent<IDamageable>(out var target))
+            _targetInRange = target;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            _targetInRange = null;
+    }
+
+    public void Attack(IDamageable target)
+    {
+        _anim.SetTrigger("attack");
+        target.TakeDamage(_damage);
+    }
 }
