c79eb67239295d95368d80d50ec9fc013ea71eb1
JlordS32 <jlordsty128@gmail.com>
2025-07-06 12:50:32 +1000

    hp ui for player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour, IDamageable
 {
+    // UI
+    [SerializeField] Image _healthBar;
+    [SerializeField] GameObject _health;
+
     [SerializeField] PlayerStatConfig _statConfig;
 
     // VARIABLES
@@ -15,6 +20,12 @@ public class Player : MonoBehaviour, IDamageable
 
     public void TakeDamage(float damage)
     {
+        if (_health != null && !_health.activeSelf)
+        {
+            _health.SetActive(true);
+        }
+
         _playerData.ApplyDamage(damage);
+        _healthBar.fillAmount = _playerData.Get(PlayerStats.HP) / _playerData.Get(PlayerStats.MaxHP);
     }
 }
