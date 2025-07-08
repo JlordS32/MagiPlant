28549d484d1f8b67e37fb3d1d7c249076539d234
JlordS32 <jlordsty128@gmail.com>
2025-07-08 10:14:37 +1000

    refactoring upgrade system

--- Assets/Scripts/RuntimeData/UpgradeEntry.cs
+++ Assets/Scripts/RuntimeData/UpgradeEntry.cs
@@ -1,12 +1,19 @@
 using System;
 using UnityEngine.Events;
 
+[System.Serializable]
 public class UpgradeEntry
 {
-    public string Name;
-    public Func<string> GetRate;
-    public Func<string> GetLevel;
-    public Func<string> GetButtonLabel;
-    public UnityAction UpgradeLogic;
-    public UpgradeUI UIRef;
+    public UpgradeType Type;
+    public float Value;
+    public int Cost;
+    public int Level;
+    public int MaxLevel;
+
+    public void Upgrade()
+    {
+        if (Level < MaxLevel) Level++;
+    }
+
+    public float GetUpgradeValue() => Value * Level;
 }
