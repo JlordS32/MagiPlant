37977b5bb44cf42cb2d7fbcc21e92f6a1bd851fd
JlordS32 <jlordsty128@gmail.com>
2025-07-18 10:08:11 +1000

    added time ui

--- Assets/Scripts/UI/UIControllers/HUDController.cs
+++ Assets/Scripts/UI/UIControllers/HUDController.cs
@@ -7,12 +7,19 @@ public class HUDController : MonoBehaviour
 
     Label _sunLabel;
     Label _waterLabel;
+    Label _timeLabel;
 
     void Start()
     {
         var root = _hudDocument.rootVisualElement;
         _sunLabel = root.Q<Label>("SunLabel");
         _waterLabel = root.Q<Label>("WaterLabel");
+        _timeLabel = root.Q<Label>("TimeLabel");
+    }
+
+    void Update()
+    {
+        _timeLabel.text = $"{TimeManager.Instance.GetTimeString()}";
     }
 
     void OnEnable() => GameEventsManager.OnCurrencyUpdate += UpdateUI;
@@ -20,7 +27,7 @@ public class HUDController : MonoBehaviour
 
     void UpdateUI(CurrencyType type, float value)
     {
-        string formatted = $"{type}: {NumberFormatter.Format(value)}";
+        string formatted = $"{NumberFormatter.Format(value)}";
         if (type == CurrencyType.Sunlight) _sunLabel.text = formatted;
         else if (type == CurrencyType.Water) _waterLabel.text = formatted;
     }
