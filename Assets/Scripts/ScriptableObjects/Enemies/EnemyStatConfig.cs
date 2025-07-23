a82720f8e4fbe8d2ff92a4b5a81ae5162df971fd
JlordS32 <jlordsty128@gmail.com>
2025-07-23 21:50:14 +1000

    updated enemy stat config

--- Assets/Scripts/ScriptableObjects/Enemies/EnemyStatConfig.cs
+++ Assets/Scripts/ScriptableObjects/Enemies/EnemyStatConfig.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 //TODO[JAYLOU]: Update the stat config so it's more configurable like Resource and Tower.
@@ -5,8 +7,15 @@ using UnityEngine;
 public class EnemyStatConfig : ScriptableObject
 {
     public int MaxLevel;
-    public float HP = 50;
-    public float Attack = 10;
-    public float Defense = 3;
-    public float Speed = 2;
+    public float BaseCost;
+    public EnemyStatEntry[] Stats;
+    public Dictionary<EnemyStats, EnemyStatEntry> _statLookup;
+
+    void InitLookup() => _statLookup ??= Stats.ToDictionary(stat => stat.Stat, stat => stat);
+
+    public float GetValue(EnemyStats stat, int level = 0)
+    {
+        InitLookup();
+        return _statLookup.TryGetValue(stat, out var entry) ? entry.GetValue(level) : 0f;
+    }
 }
